656334504d1773a14ad456161dc4ceedb413a606
Winter <1693703607@qq.com>
2022-10-20 09:53:11 +0800

    get NBA link

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,9 +14,9 @@ public class Main {
         String link = "https://www.qq.com/";
         try {
             URL url = new URL(link);
-            InputStream inputStream = url.openStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "gbk"));
             String line, content;
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "gbk"));
             StringBuilder stringBuilder = new StringBuilder();
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
@@ -33,10 +33,14 @@ public class Main {
                 item.put(new JSONObject().put("href", m.group(1)));
                 jsonArray.put(item);
             }
+            //生成数组
             System.out.println(jsonArray);
+            reader.close();
+
+            //生成csv文件
             File csvFile = new File("E:/qq_nav.csv");
             csvFile.createNewFile();
-            BufferedWriter csvWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csvFile), "gbk"), 1024);
+            BufferedWriter csvWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csvFile), "gbk"));
 
             for (int i=0; i < jsonArray.length(); i++) {
                 JSONArray item = new JSONArray(jsonArray.get(i).toString());
@@ -68,6 +72,20 @@ public class Main {
                 }
             }
             csvWriter.flush();
+            csvWriter.close();
+
+            //读取csv文件
+            BufferedReader csvReader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile), "gbk"));
+            while ((line = csvReader.readLine()) != null) {
+                String[] splitArray = line.split(",");
+                if ("NBA".equals(splitArray[0])) {
+                    //获取NBA频道的链接
+                    System.out.println(splitArray[1]);
+                    break;
+                }
+            }
+
+            //扩展
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
